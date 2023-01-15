0deb9ca7e34c22e73de6dd029d07b0a024c18f73
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-14 22:58:53 -0600

    Section 7 Adding HostListener

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
+  
   constructor(private elRef:ElementRef,  private renderer: Renderer2) { }
   ngOnInit(): void {
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    // this.renderer.setStyle(this.elRef.nativeElement,'color','white');
+  }
+  @HostListener('mouseenter') mouseover(eventData: Event){
     this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
     this.renderer.setStyle(this.elRef.nativeElement,'color','white');
   }
-
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    this.renderer.setStyle(this.elRef.nativeElement,'color','black');
+  }
 }
